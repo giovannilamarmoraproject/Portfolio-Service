7155f1e8a1c8343ca6bf96095d839d309beecc5f
Giovanni Lamarmora <giovannilamarmora.working@gmail.com>
2023-12-20 21:01:10 +0100

    initial commit

---
+++ src/test/java/com/giovannilamarmora/project/portfolio/PortfolioService/PortfolioServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.giovannilamarmora.project.portfolio.PortfolioService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortfolioServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
